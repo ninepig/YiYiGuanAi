99bdb845d720905a429434b6114d715012190189
wenjing yang <yangwenjingshogun@gmail.com>
2016-02-24 10:29:08 -0500

    change uiactivity ,add button function

--- mobile/src/main/java/com/example/yamengwenjing/yiyiguanai/UIActivity.java
+++ mobile/src/main/java/com/example/yamengwenjing/yiyiguanai/UIActivity.java
@@ -2,6 +2,7 @@ package com.example.yamengwenjing.yiyiguanai;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UIActivity extends AppCompatActivity {
 
@@ -9,9 +10,18 @@ public class UIActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ui);
-
-
-
+    }
+    public void gotoMoa(View view){
 
     }
+
+
+    public void personalSetting(View view){
+
+    }
+
+    public void sychWithCloud(View view){
+
+    }
+
 }
